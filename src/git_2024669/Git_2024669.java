3cd5c6256b43d379fae0a9acdd1985ea398cc8a0
shahzaib5656 <2024669@student.cct.ie>
2024-12-31 13:02:04 +0000

    Added a simple print statement.

--- src/git_2024669/Git_2024669.java
+++ src/git_2024669/Git_2024669.java
@@ -15,6 +15,8 @@ public class Git_2024669 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Shahzaib Hassan - 2024669");
     }
     
 }
